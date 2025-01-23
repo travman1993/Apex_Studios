26962bd65a10bb57b76f1724d11a98dd56848875
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-01-22 19:15:38 -0500

    added logo img, menu icon, js for the buttons and drop down menu, last thing is making a video for the vid spot

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("quoteButton").onclick = function () {
+    window.location.href = "quote.html";
+};
+
+document.getElementById("portfolioButton").addEventListener("click", function () {
+    window.location.href = "portfolio.html";
+});
+
+document.getElementById("contactButton").addEventListener("click", function () {
+    window.location.href = "contact.html";
+});
+
+document.getElementById("servicesButton").addEventListener("click", function () {
+    window.location.href = "services.html";
+});
+
+document.getElementById("homeButton").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
---
+++ menu.js
@@ -0,0 +1,9 @@
+document.getElementById("menuIcon").onclick = function() {
+    const menu = document.getElementById("menu");
+
+    if (menu.style.display === "none" || menu.style.display === "") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
